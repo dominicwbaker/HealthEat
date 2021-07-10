19c9aca2cf26a1478473857ebfbe01a6b1393c43
roybduvall <roybduvall@gmail.com>
2021-07-10 11:38:31 -0500

    changing views folder to client adding controllers for menu and user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,11 +12,18 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/healtheat");
+
+// Add routes, both API and view
+const routes = require("./routes");
+app.use(routes);
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
