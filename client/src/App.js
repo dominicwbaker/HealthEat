f38cfc9bfe26156fa9e0de0a874acb4e41b2e8fb
roybduvall <roybduvall@gmail.com>
2021-07-10 13:26:04 -0500

    ninos changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/signup" component={Signup} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
