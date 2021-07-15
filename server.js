d2d8c70d50f5e2f262e0e912b373da6d7bae5bfc
roybduvall <roybduvall@gmail.com>
2021-07-14 19:32:17 -0500

    updates routes to allow writing to db

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/healtheat");
 
 // Add routes, both API and view
-// const routes = require("./routes");
-// app.use(routes);
+const routes = require("./routes");
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Start the API server
