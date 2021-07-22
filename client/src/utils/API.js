257a620138f7aa64c3beefdd524346e4fc96789d
roybduvall <roybduvall@gmail.com>
2021-07-21 19:22:07 -0500

    toying with authentification

--- client/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the edamam API
-
-export default {
-  search: function(query) {
-    return axios.get(`/search?q=${query}`);
-  }
-}
